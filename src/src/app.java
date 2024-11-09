1f187765b63935d2744fe2a72f74ad8ce0d003a4
Legrand <legrandd@buffalo.edu>
2024-11-09 16:45:05 -0500

    initialized window

--- src/src/app.java
+++ src/src/app.java
@@ -1,18 +1,48 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class app {
 
     private static void initWindow() {
-        JFrame window = new JFrame("this is the window i guess");
+
+        JFrame window = new JFrame("Our Boba Game!!! :3");
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-
-        window.setLocationRelativeTo(null);
+        window.setResizable(false);
+        window.add(new GamePanel()); //see GamePanel,
+        window.pack(); //sets window size to specified size in GamePanel
+        window.setLocationRelativeTo(null); //put window in the middle of the screen
         window.setVisible(true);
+
+
     }
 
     public static void main(String[] args) {
         initWindow();
     }
 
+
+
+}
+class GamePanel extends JPanel implements Runnable{ //RyiSnow on YouTube ty ty
+
+    final int tileSize = 48; //each 1 tile is 48x48 pixels, final means it can't be changed
+    final int screenHeight = 12 * tileSize; //576 pixels tall // 12 tiles tall, temp size
+    final int screenWidth = 16 * tileSize; //768 pixels wide // 16 tiles tall, temp size
+
+    Thread gameThread;
+    public GamePanel(){
+        this.setPreferredSize(new Dimension(screenWidth, screenHeight));
+        this.setBackground(Color.pink);
+        this.setDoubleBuffered(true);
+    }
+
+    public void startGameThread(){
+        gameThread = new Thread(this);
+        gameThread.start();
+    }
+
+    @Override
+    public void run(){
+
+    }
 }
